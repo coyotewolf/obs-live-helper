3c2d128ba61d428233c1b63fcc8b2d86f77b6981
coyotewolf <coyote2025.0723@gmail.com>
2025-08-08 20:52:26 +0800

    step5-1: orginal dashboard layout

--- server.js
+++ server.js
@@ -4,19 +4,14 @@ const path = require('path');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 5172;
 
 app.use(cors());
 app.use(express.json());
 
-// 靜態檔：前端
 app.use(express.static(path.join(__dirname, 'public')));
-
-// 靜態檔：LRC 歌詞
 app.use('/lyrics', express.static(path.join(__dirname, 'lyrics')));
 
 app.use('/api', require('./routes'));
-
 
 const { startLyricSync } = require('./services/lyricsFetcher');
 startLyricSync();
