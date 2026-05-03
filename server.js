61be1ecc3ef4f195da83ae7e702442a2a9ef5cbc
coyotewolf <46484080+coyotewolf@users.noreply.github.com>
2026-05-03 17:07:44 +0800

    Inject dashboard goal reorder script

--- server.js
+++ server.js
@@ -241,6 +241,9 @@ app.get('/html/dashboard.html', (req, res, next) => {
     if (!html.includes('dashboard-settings-extra.js')) {
       html = html.replace('</body>', '<script src="../js/dashboard-settings-extra.js"></script>\n</body>');
     }
+    if (!html.includes('dashboard-goal-reorder-extra.js')) {
+      html = html.replace('</body>', '<script src="../js/dashboard-goal-reorder-extra.js"></script>\n</body>');
+    }
     if (!html.includes('dashboard-onboarding-extra.js')) {
       html = html.replace('</body>', '<script src="../js/dashboard-onboarding-extra.js"></script>\n</body>');
     }
