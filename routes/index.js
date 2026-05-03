15c73b64c9693833929c974d30c85745f1034aab
coyotewolf <46484080+coyotewolf@users.noreply.github.com>
2026-05-03 11:36:54 +0800

    Register backup API route

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ router.use('/style', require('./style'));
 router.use('/request', require('./request'));
 router.use('/discord-profile', require('./discordProfile'));
 router.use('/overlay-config', require('./overlayConfig'));
+router.use('/backup', require('./backup'));
 
 router.get('/hello', (req, res) => {
   res.json({ message: 'API is working! 👋' });
