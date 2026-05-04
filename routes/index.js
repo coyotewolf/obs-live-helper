ca7c49f5805e61c32df0ae8ad2f9a51d5d2ffd18
coyotewolf <46484080+coyotewolf@users.noreply.github.com>
2026-05-04 18:59:46 +0800

    Mount interruption management API

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ router.use('/spotify', require('./spotify'));
 router.use('/editor', require('./editor'));
 router.use('/style', require('./style'));
 router.use('/request', require('./request'));
+router.use('/interruption', require('./interruption'));
 router.use('/discord-profile', require('./discordProfile'));
 router.use('/overlay-config', require('./overlayConfig'));
 router.use('/backup', require('./backup'));
